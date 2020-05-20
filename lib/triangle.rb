e3bca3ff5b9d9b9122363fdf892b32b98f61911a
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 18:57:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     if (@side_a <= 0) || (@side_b <= 0) || (@side_a <= 0)
       raise TriangleError
     elsif (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
-      
+
     end
   else
     if @side_a == @side_b && @side_a == @side_c
