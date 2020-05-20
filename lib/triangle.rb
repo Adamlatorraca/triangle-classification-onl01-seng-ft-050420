7eb89502b80429c00c1a0ff03ec6219607d204c1
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 18:55:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,10 @@ class Triangle
     if (@side_a <= 0) || (@side_b <= 0) || (@side_a <= 0)
       raise TriangleError
     end
-  if @side_a == @side_b && @side_a == @side_c
+  elsif condition
+
+  else
+    if @side_a == @side_b && @side_a == @side_c
       return :equilateral
     elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
       return :isosceles
