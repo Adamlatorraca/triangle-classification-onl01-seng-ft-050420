45aab3a672b8e3dfd61f6dd0350b07f9668ce40b
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 18:20:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(attributes=nil)
-    if attribuse
+    if attributes
     attributes.each do |k,v|
       self.send("#{k}=", v)
     end
